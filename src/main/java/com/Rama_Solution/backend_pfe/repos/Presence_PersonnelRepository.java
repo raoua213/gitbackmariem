86f99d083f9d2c4816cb366a8d72b5de45af56c7
unknown <mariemdabouna@gmail.com>
2022-05-22 17:36:37 +0100

    back

--- src/main/java/com/Rama_Solution/backend_pfe/repos/Presence_PersonnelRepository.java
+++ src/main/java/com/Rama_Solution/backend_pfe/repos/Presence_PersonnelRepository.java
@@ -16,6 +16,6 @@ public interface Presence_PersonnelRepository extends JpaRepository<Presence_per
 	@Query("SELECT pp FROM  Presence_personnel pp WHERE pp.etat=:etat")
 	List<Presence_personnel> findByEtat(@Param("etat") Boolean etat);
 	
-	@Query("SELECT count(*) FROM Presence_personnel pp WHERE pp.presence.idPersonnel= :idPersonnel and pp.date between :dateDebut and :dateFin")
+	@Query("SELECT count(*) FROM Presence_personnel pp WHERE pp.presence.idPersonnel= :idPersonnel and pp.date between :dateDebut and :dateFin and pp.etat= false")
 	int findAllAbsentByIdPersonnel_Date(@Param("idPersonnel") Long idPersonnel, @Param("dateDebut") Date dateDebut, @Param("dateFin") Date dateFin);
 }
